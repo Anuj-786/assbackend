7b288499d5bea29f3ed15dbc591651ed8ac89b12
Anuj-786 <anuj.k@gai.co.in>
2018-05-07 21:13:46 +0530

    adding routes

--- routes/getbookmark.js
+++ routes/getbookmark.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.post('/', function(req, res, next) {
-  connection.query('SELECT * from bookmark WHERE user_id = "' + req.body.user_id + '"', function (error, results, fields) {
+router.get('/', function(req, res, next) {
+  connection.query('SELECT * from bookmark', function (error, results, fields) {
 	  	if(error){
 	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
 	  		//If there is error, we send the error in the error section with 500 status
@@ -11,8 +11,6 @@ router.post('/', function(req, res, next) {
   			res.send({"status": 200, "error": null, "response": results});
   			//If there is no error, all is good and response is 200OK.
 	  	}
-      console.log(res.body)
-      console.log(req.body)
   });
 });
 
